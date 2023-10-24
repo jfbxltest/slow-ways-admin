2068fe78f9049bf5afaea7c8cdd8228481eac039
1402jelorand <jeanfebxl@hotmail.fr>
2023-10-24 20:57:22 +0200

    clean up

--- travail-dev/webComponent/js/components/OptionsElements.js
+++
@@ -1,7 +0,0 @@
-export default class OptionsElement extends HTMLElement {
-  constructor(data) {
-    super();
-
-    console.log("on constuct options");
-  }
-}
--- travail-dev/webComponent/js/test.js
+++
@@ -1,3 +0,0 @@
-function loadActivityByIdTest() {
-  console.log("ok *******");
-}
